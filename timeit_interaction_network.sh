1aa68aabaefdc23b9fe7f7832c263eb8d700cee4
Javier Duarte <jduarte@ucsd.edu>
2021-03-03 17:44:52 -0800

    update

--- timeit_interaction_network.sh
+++ timeit_interaction_network.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-PT="2GeV"
+PT="1GeV"
 BATCHSIZE=1
 GRAPHBATCHNUM=0
 CONSTRUCTION="heptrkx_plus"
 CUDA=1
 SETUP="import os
-os.environ['CUDA_VISIBLE_DEVICES']='0' # no gpus
+os.environ['CUDA_VISIBLE_DEVICES']='0' # pick gpu
 import torch
+torch.set_grad_enabled(False)
 from torchvision import datasets, transforms
 import numpy as np
 from models.dataset import Dataset
@@ -22,16 +23,17 @@ n_graphs = len(graph_files)
 IDs = np.arange(n_graphs)
 np.random.shuffle(IDs)
 partition = {'test':  graph_files[IDs[:100]]}
-params = {'batch_size': $BATCHSIZE, 'shuffle': True, 'num_workers': 0}
+params = {'batch_size': $BATCHSIZE, 'shuffle': False, 'num_workers': 0}
 test_set = Dataset(graph_indir, partition['test'])
 test_loader = torch.utils.data.DataLoader(test_set, **params)
 model = InteractionNetwork(3, 4, 4).to(device)
 model.eval()
-torch.no_grad()
-data, target =  list(test_loader)[$GRAPHBATCHNUM]
+i = 0
+for data, target in test_loader:
+  if i == $GRAPHBATCHNUM: break
+  i += 1
 X, Ra = data['X'].to(device), data['Ra'].to(device).float()
 Ri, Ro = data['Ri'].to(device).float(), data['Ro'].to(device).float()"
 echo $SETUP
 python -m timeit -s "$SETUP" -n 100 -r 5 -v "model(X, Ra, Ri, Ro)"
-
 
